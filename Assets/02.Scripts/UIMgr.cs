39321ec55ce4e4c09a0d529099e515f3d072d7ea
kimtaeyun0419 <sdh230104@sdh.hs.kr>
2024-04-28 01:57:25 +0900

    UI Setting_1

---
+++ Assets/02.Scripts/UIMgr.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMgr : MonoBehaviour
+{
+    public void OnbuttonClick()
+    {
+        Debug.Log($"Click Button : ");
+    }
+}
